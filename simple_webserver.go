d31d7cb591f979452b9df7eb491ac93baff47728
FendiJatmiko <jatmikoFendi@gmail.com>
2016-05-19 11:41:06 +0700

    a lil rev for docs and simple_webserver.go

--- simple_webserver.go
+++ simple_webserver.go
@@ -5,14 +5,18 @@ import (
 )
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
-
-  fmt.Fprintln(w, "<h1>#############</h1>")
   fmt.Fprintln(w, "<p>this is paragraph</p>")
   fmt.Fprintln(w, "<h2>Hello, World!</h2>")
-  fmt.Fprintln(w, "<h1>#############</h1>")
+}
+
+func nameHandler(w http.ResponseWriter, r *http.Request) {
+  hallo := r.FormValue("hallo")
+  nama  := r.FormValue("nama")
+  fmt.Fprintf(w, "<h1>%s, %s!</h1>", hallo, nama)
 }
 
 func main() {
   http.HandleFunc("/", RootHandler)
+  http.HandleFunc("/nama", nameHandler)
   http.ListenAndServe(":8000", nil)
 }
